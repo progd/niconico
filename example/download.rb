36f7b7e46edc0f99c17ffe5fc01be175d2cec838
progd <gprogd@gmail.com>
2013-05-04 21:40:54 +0900

    add example

---
+++ example/download.rb
@@ -0,0 +1,14 @@
+require_relative '../lib/niconico'
+
+nico = Niconico.new('email', 'password')
+nico.agent.request_headers = {
+  'accept-language' => 'ja, ja-JP, en',
+  'accept-encoding' => 'utf-8, us-ascii'
+}
+video = nico.video('sm9')
+
+puts "title=#{video.title}"
+open("#{video.title}.#{video.type}", 'w') { |file|
+  file.print video.get_video
+}
+
